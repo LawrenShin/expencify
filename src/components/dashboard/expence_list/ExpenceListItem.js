649b130f6bd198921e913f0aa946125b111385a9
LawrenShin <simplyforgit@gmail.com>
2018-09-25 12:17:07 +0300

    formatting date and nums

--- src/components/dashboard/expence_list/ExpenceListItem.js
+++ src/components/dashboard/expence_list/ExpenceListItem.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenceListItem = ({ dispatch, id, description, amount, createdAt, note }) => {
   return (
     <li>
       <NavLink to={`/edit/${id}`} ><h3>{description}</h3></NavLink>
-      <span>{amount}</span>
-      <span>{createdAt}</span>
-      <span>{note}</span>
+      <p>
+        <span>{numeral(amount / 100).format('$0,0.00')}</span><br/>
+        <span>{moment(createdAt).format('Do/MMM/YYYY')}</span>
+        <span>{note}</span>
+      </p>
     </li>
   );
 };
